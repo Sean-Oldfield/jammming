8dabdbd90595d6786db6312118973306adb6e099
Sean Oldfield <seanoldfield95@gmail.com>
2021-05-11 12:54:22 +0100

    spotify integration and search working

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,16 +3,65 @@ import './App.css';
 import { SearchBar } from '../SearchBar/SearchBar.js';
 import { SearchResults } from '../SearchResults/SearchResults.js';
 import { Playlist } from '../Playlist/Playlist.js';
+import { Spotify } from '../../util/Spotify.js';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { searchResults: [{
-            name: 'River Island Theme',
-            artist: 'Sean Oldfield',
-            album: 'Xmas Bangers',
-            id: 1
-        }] };
+        this.state = { 
+            searchResults: [{
+                name: 'River Island Theme',
+                artist: 'Sean Oldfield',
+                album: 'Xmas Bangers',
+                id: 1
+            }],
+            playListName: 'Banging Tunes',
+            playListTracks: [{
+                name: 'River Island Theme',
+                artist: 'Sean Oldfield',
+                album: 'Xmas Bangers',
+                id: 1
+            }],
+        };
+        this.addTrack = this.addTrack.bind(this);
+        this.removeTrack = this.removeTrack.bind(this);
+        this.updatePlaylistName = this.updatePlaylistName.bind(this);
+        this.savePlaylist = this.savePlaylist.bind(this);
+        this.search = this.search.bind(this);
+    }
+
+    addTrack(track) {
+        if (this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
+            return;
+        }
+        let newPlayListTracks = this.state.playListTracks.push(track);
+        this.setState({ playListTracks: newPlayListTracks });
+    }
+
+    removeTrack(track) {
+        let currentTracks = this.state.playListTracks;
+        let newPlayListTracks = currentTracks.filter(savedTrack => savedTrack.id !== track.id);
+        this.setState({ playListTracks: newPlayListTracks });
+    }
+
+    updatePlaylistName(name) {
+        this.setState({ playListName: name });
+    }
+
+    savePlaylist() {
+        let trackURIs = [];
+        let playListTracks = this.state.playListTracks;
+        for (let i = 0; i < playListTracks.length; i++) {
+            trackURIs.push(playListTracks[i].uri);
+        }
+    }
+
+    search(term) {
+        Spotify.search(term).then(searchResults => {
+			this.setState({
+				searchResults: searchResults
+			})
+		});
     }
         
     render() {
@@ -20,10 +69,10 @@ class App extends React.Component {
             <div>
                 <h1>Ja<span className="highlight">mmm</span>ing</h1>
                 <div className="App">
-                    {/* Add a SearchBar component */}
+                    <SearchBar onSearch={this.search} />
                     <div className="App-playlist">
-                        <SearchResults searchResults={this.state.searchResults} />
-                        {/* Add a Playlist component */}
+                        <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
+                        <Playlist playListName={this.state.playListName} playListTracks={this.state.playListTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist}/> 
                     </div>
                 </div>
             </div>
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,13 +2,29 @@ import React from 'react';
 import './Track.css';
 
 export class Track extends React.Component {
+    constructor(props) {
+        super(props);
+        this.addTrack = this.addTrack.bind(this);
+        this.removeTrack = this.removeTrack.bind(this);
+    }
+
     renderAction() {
         let plusOrMinus = this.props.isRemoval;
         if (plusOrMinus) {
-            return <button className="Track-action">-</button>;
+            return <button className="Track-action" onClick={this.removeTrack}>-</button>;
         } else {
-            return <button className="Track-action">+</button>;
+            return <button className="Track-action" onClick={this.addTrack}>+</button>;
         }
+    }
+
+    addTrack() {
+        let addTrack = this.props.onAdd;
+        addTrack(this.props.track);
+    }
+
+    removeTrack() {
+        let removeTrack = this.props.onRemove;
+        removeTrack(this.props.track);
     }
     
     render() {
